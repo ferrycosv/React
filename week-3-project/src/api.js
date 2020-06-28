afa1d5a7b94d59b625c3aac36450885d494da5df
Francisco Fernández <ferrycosv@gmail.com>
2020-06-28 17:33:35 +0200

    Week 3 exercises completed

--- week-3-project/src/api.js
+++ week-3-project/src/api.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 
 // Exercise Pokedex:
 // A pokedex is a machine that displays all the known pokemon
@@ -7,119 +7,125 @@ import React, {useEffect, useState} from "react";
 // TODO: render the names of the pokemons
 
 class Pokedex extends React.Component {
+  state = {
+    pokemons: [],
+  };
 
-    state = {
-        pokemons: []
-    }
+  componentDidMount() {
+    const fetchPokemons = () => {
+      return fetch("https://pokeapi.co/api/v2/pokedex/2/")
+        .then((response) => response.json())
+        .then((json) => json.pokemon_entries);
+    };
 
-    componentDidMount() {
-        const fetchPokemons = () => {
-            return fetch('https://pokeapi.co/api/v2/pokedex/2/')
-                .then(response => response.json())
-                .then(json => json.pokemon_entries);
-        };
+    fetchPokemons().then((pokemons) => {
+      // Set the state here
+      this.setState({ pokemons: pokemons });
+    });
+  }
 
-        fetchPokemons()
-            .then((pokemons) => {
-                // Set the state here
-            });
-    }
-
-    render() {
-        return (
-            <div className={'pokedex'}>
-                <h2>Pokedex</h2>
-                {
-                    this.state.pokemons.map(pokemon => <Pokemon key={pokemon.entry_number} {...pokemon} />)
-                }
-            </div>
-        )
-    }
-
+  render() {
+    return (
+      <div className={"pokedex"}>
+        <h2>Pokedex</h2>
+        {this.state.pokemons.map((pokemon) => (
+          <Pokemon key={pokemon.entry_number} {...pokemon} />
+        ))}
+      </div>
+    );
+  }
 }
 
 class Pokemon extends React.Component {
-    render() {
-        return (
-            <article>
-                {/* Render the property here */}
-            </article>
-        )
-    }
+  render() {
+    return (
+      <article>
+        {this.props.pokemon_species.name /* Render the property here */}
+      </article>
+    );
+  }
 }
 
 class InteractivePokedex extends React.Component {
+  state = {
+    pokemons: [],
+    selectedPokemon: false,
+  };
 
-    state = {
-        pokemons: [],
-        selectedPokemon: false
-    }
-
-    onSelectHandler = (pokemon) => {
-        const fetchPokemon = () => {
-            return fetch(pokemon.url)
-                .then(response => response.json());
-        };
-
-        /* Use the result of the fetchPokemon function */
-        /* set the result using selectedPokemon, be sure to support the render below */
+  onSelectHandler = (pokemon) => {
+    const fetchPokemon = () => {
+      return fetch(pokemon.url).then((response) => response.json());
     };
 
-    componentDidMount() {
-        const fetchPokemons = () => {
-            return fetch('https://pokeapi.co/api/v2/pokedex/2/')
-                .then(response => response.json())
-                .then(json => json.pokemon_entries);
-        };
+    /* Use the result of the fetchPokemon function */
+    /* set the result using selectedPokemon, be sure to support the render below */
+    fetchPokemon().then((pokemon) =>
+      this.setState({ pokemons: this.state.pokemons, selectedPokemon: pokemon })
+    );
+  };
 
-        fetchPokemons()
-            .then((pokemons) => {
-                // Set the state here
-            });
-    }
+  componentDidMount() {
+    const fetchPokemons = () => {
+      return fetch("https://pokeapi.co/api/v2/pokedex/2/")
+        .then((response) => response.json())
+        .then((json) => json.pokemon_entries);
+    };
 
-    render() {
-        return (
-            <div className={'pokedex'}>
-                <h2>Interactive Pokedex</h2>
-                {
-                    this.state.selectedPokemon === false
-                        ? (
-                            this.state.pokemons.map(pokemon => <InterActivePokemon key={pokemon.entry_number} {...pokemon} /* pass the onSelectHandler here a property */ />)
-                        )
-                        : (
-                            <DetailedPokemon {...this.state.selectedPokemon} />
-                        )
-                }
-            </div>
-        )
-    }
+    fetchPokemons().then((pokemons) => {
+      // Set the state here
+      this.setState({ pokemons: pokemons, selectedPokemon: false });
+    });
+  }
+
+  render() {
+    return (
+      <div className={"pokedex"}>
+        <h2>Interactive Pokedex</h2>
+        {this.state.selectedPokemon === false ? (
+          this.state.pokemons.map((pokemon) => (
+            <InterActivePokemon
+              key={pokemon.entry_number}
+              {...pokemon}
+              onSelectHandler={
+                this.onSelectHandler
+              } /* pass the onSelectHandler here a property */
+            />
+          ))
+        ) : (
+          <DetailedPokemon {...this.state.selectedPokemon} />
+        )}
+      </div>
+    );
+  }
 }
 
 class DetailedPokemon extends React.Component {
-    render() {
-        return (
-            <article>
-                {this.props.flavor_text_entries.map((entry, index) => <p key={index}>{entry.flavor_text}</p>)}
-            </article>
-        )
-    }
+  render() {
+    return (
+      <article>
+        {this.props.flavor_text_entries.map((entry, index) => (
+          <p key={index}>{entry.flavor_text}</p>
+        ))}
+      </article>
+    );
+  }
 }
 
 class InterActivePokemon extends React.Component {
+  onClick = () => {
+    /* trigger the onSelectedHandler function with the pokemon_species */
+    debugger;
+    this.props.onSelectHandler(this.props.pokemon_species);
+  };
 
-    onClick = () => {
-        /* trigger the onSelectedHandler function with the pokemon_species */
-    };
-
-    render() {
-        return (
-            <article>
-                {/* Render the property here */}
-                <button onClick={this.onClick}>Learn more</button>
-            </article>
-        )
-    }
+  render() {
+    return (
+      <article>
+        {this.props.pokemon_species.name /* Render the property here */}
+        <button onClick={this.onClick}>Learn more</button>
+      </article>
+    );
+  }
 }
 
 export { Pokedex, InteractivePokedex };
--- week-3-project/src/forms.js
+++ week-3-project/src/forms.js
@@ -1,6 +1,6 @@
-import React, {useState} from 'react';
+import React, { useState } from "react";
 
-import './forms.css'
+import "./forms.css";
 
 // Exercise: Contact Form
 // We are looking to capture user feedback, for this we add a contact from
@@ -8,53 +8,60 @@ import './forms.css'
 // TODO: Thank the user after completing the form
 
 class ContactForm extends React.Component {
+  state = {
+    comment: "",
+    name: "",
+    submittedText: "",
+  };
 
-    state = {
-        comment: '',
-        name: '',
-        submittedText: ''
-    }
+  onSubmit = (event) => {
+    event.preventDefault(); // We disable the default behaviour of a form
+    this.setState({
+      submittedText: `Thanks for submitting ${this.state.name}`,
+    });
+  };
 
-    onSubmit = (event) => {
-        event.preventDefault(); // We disable the default behaviour of a form
-        this.setState({
-            submittedText: `Thanks for submitting ${this.state.name}`
-        });
-    };
-
-    render() {
-        return (
-            <section className="contact">
-                { !this.state.submittedText && (
-                    <form onSubmit={this.onSubmit}>
-                        <div className="field">
-                            <label htmlFor="comment">Your question or comment</label> <br />
-                            <textarea
-                                id="comment"
-                                name="comment"
-                                onChange={(event) => { /* use event.target.value to set the comment */ }}
-                            />
-                        </div>
-                        <div className="field">
-                            <label htmlFor="email">Name</label> <br />
-                            <input
-                                id="name"
-                                type="text"
-                                name="name"
-                                aria-label="contact-name"
-                                onChange={(event) => { /* use event.target.value to set the name */ }}
-                            />
-                        </div>
-                        <button type="submit">Send it!</button>
-                    </form>
-                )}
-                { this.state.submittedText && (
-                    <p>{this.state.submittedText}</p>
-                )}
-            </section>
-        )
-    };
-};
+  render() {
+    return (
+      <section className="contact">
+        {!this.state.submittedText && (
+          <form onSubmit={this.onSubmit}>
+            <div className="field">
+              <label htmlFor="comment">Your question or comment</label> <br />
+              <textarea
+                id="comment"
+                name="comment"
+                onChange={(event) => {
+                  this.setState({
+                    comment: event.target.value,
+                  });
+                  /* use event.target.value to set the comment */
+                }}
+              />
+            </div>
+            <div className="field">
+              <label htmlFor="email">Name</label> <br />
+              <input
+                id="name"
+                type="text"
+                name="name"
+                aria-label="contact-name"
+                onChange={(event) => {
+                  this.setState({
+                    name: event.target.value,
+                  });
+                  /* use event.target.value to set the name */
+                }}
+              />
+            </div>
+            <button type="submit">Send it!</button>
+          </form>
+        )}
+        {this.state.submittedText && <p>{this.state.submittedText}</p>}
+      </section>
+    );
+  }
+}
 
 // Exercise: Math Form
 // We are making a basic calculator that count up numbers
@@ -62,45 +69,52 @@ class ContactForm extends React.Component {
 // TODO: Fix the issue with the number not counting up correctly
 
 class MathForm extends React.Component {
+  state = {
+    numberA: 0,
+    numberB: 0,
+  };
 
-    state = {
-        numberA: 0,
-        numberB: 0
-    }
+  render() {
+    // numberA and numberB are strings how can we force them to be numbers ?
+    const sum = this.state.numberA + this.state.numberB;
 
-    render() {
-        // numberA and numberB are strings how can we force them to be numbers ?
-        const sum = this.state.numberA + this.state.numberB;
+    return (
+      <section className="contact">
+        <form>
+          <div className="field">
+            <input
+              id="name"
+              type="number"
+              name="numberA"
+              aria-label="math-number-a"
+              onChange={(event) => {
+                this.setState({
+                  numberA: Number(event.target.value),
+                });
+              }}
+              /* add an attribute with value here that causes numberA to update when the input changes */
+            />
+            &nbsp; + &nbsp;
+            <input
+              id="name"
+              type="number"
+              name="numberB"
+              aria-label="math-number-b"
+              onChange={(event) => {
+                this.setState({
+                  numberB: Number(event.target.value),
+                });
+              }}
+              /* add an attribute with value here that causes numberA to update when the input changes */
+            />
+          </div>
+        </form>
 
-        return (
-            <section className="contact">
-                <form>
-                    <div className="field">
-                        <input
-                            id="name"
-                            type="number"
-                            name="numberA"
-                            aria-label="math-number-a"
-                            /* add an attribute with value here that causes numberA to update when the input changes */
-                        />
-                        &nbsp; + &nbsp;
-                        <input
-                            id="name"
-                            type="number"
-                            name="numberA"
-                            aria-label="math-number-b"
-                            /* add an attribute with value here that causes numberA to update when the input changes */
-                        />
-                    </div>
-                </form>
-
-                <p>
-                    The sum of a and b is {sum}
-                </p>
-            </section>
-        )
-    }
-};
+        <p>The sum of a and b is {sum}</p>
+      </section>
+    );
+  }
+}
 
 // Exercise: Fruit Form
 // User can submit their favourite fruit in our form
@@ -109,67 +123,79 @@ class MathForm extends React.Component {
 // TODO: if an invalid fruit (not "banana" or "apple") is typed in an error message is displayed
 
 class FruitForm extends React.Component {
+  state = {
+    fruits: [],
+    fruit: "",
+  };
 
-    state = {
-        fruits: [],
-        fruit: ''
-    }
+  onSubmit = (event) => {
+    event.preventDefault(); // We disable the default behaviour of a form
 
-    onSubmit = (event) => {
-        event.preventDefault(); // We disable the default behaviour of a form
+    /* Only update the state of fruit if we have no errors */
+    if (event.target.childNodes.length < 3)
+      this.setState({
+        fruits: [...this.state.fruits, this.state.fruit],
+      });
+  };
 
-        /* Only update the state of fruit if we have no errors */
-        this.setState({
-            fruits: [...this.state.fruits, this.state.fruit]
-        })
-    };
+  setFruit = (fruit) => {
+    this.setState({
+      fruit: fruit,
+    });
+  };
 
-    setFruit = (fruit) => {
+  onChange = (event) => {
+    this.setFruit(event.target.value);
+  };
 
-    }
-
-    render() {
-        const error = false; /* Add an expression here that validates if fruit is a banana or apple */
-
-        return (
-            <section className="contact">
-                <form onSubmit={this.onSubmit}>
-                    <div className="field">
-                        <label htmlFor="email">Banana or Apple</label> <br />
-                        <input
-                            id="name"
-                            type="text"
-                            name="name"
-                            aria-label="fruit-name"
-                            onChange={(event) => { this.setFruit(event.target.value) }}
-                        />
-                    </div>
-                    {error && (
-                        <p className="error">I don't recognize this fruit please make a choice between "banana" or "apple"</p>
-                    )}
-                    <button type="submit">Add</button>
-                </form>
-                <ul>
-                    {this.state.fruits.map((fruit, index) => {
-                        return <li key={index}><Fruit name={fruit} /></li>
-                    })}
-                </ul>
-            </section>
-
-        )
-    }
-};
+  render() {
+    const error = ["apple", "banana"].includes(this.state.fruit)
+      ? false
+      : true; /* Add an expression here that validates if fruit is a banana or apple */
+    return (
+      <section className="contact">
+        <form onSubmit={this.onSubmit}>
+          <div className="field">
+            <label htmlFor="email">Banana or Apple</label> <br />
+            <input
+              id="name"
+              type="text"
+              name="name"
+              aria-label="fruit-name"
+              onChange={this.onChange}
+            />
+          </div>
+          {error && (
+            <p className="error">
+              I don't recognize this fruit please make a choice between "banana"
+              or "apple"
+            </p>
+          )}
+          <button type="submit">Add</button>
+        </form>
+        <ul>
+          {this.state.fruits.map((fruit, index) => {
+            return (
+              <li key={index}>
+                <Fruit name={fruit} />
+              </li>
+            );
+          })}
+        </ul>
+      </section>
+    );
+  }
+}
 
 const Fruit = ({ name }) => {
-    let fruitMoji;
-    if (name === 'banana') {
-        fruitMoji = '🍌';
-    }
-    else if (name === 'apple') {
-        fruitMoji = '🍎'
-    }
+  let fruitMoji;
+  if (name === "banana") {
+    fruitMoji = "🍌";
+  } else if (name === "apple") {
+    fruitMoji = "🍎";
+  }
 
-    return <span data-testid="fruit">{fruitMoji}</span>
+  return <span data-testid="fruit">{fruitMoji}</span>;
 };
 
 export { ContactForm, MathForm, FruitForm };
